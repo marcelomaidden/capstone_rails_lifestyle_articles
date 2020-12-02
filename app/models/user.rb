d154e73280d9ace5478719f41372634774021c93
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2020-12-02 14:07:58 -0300

    Create Article model, add its validations and relationship with User with author_id

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   validates :name, :username, presence: :true
+
+  has_many :articles, foreign_key: :author_id, source: :articles
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_154358) do
+ActiveRecord::Schema.define(version: 2020_12_02_160150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.integer "author_id"
+    t.string "title"
+    t.text "text"
+    t.text "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
