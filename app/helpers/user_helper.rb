cc474132707288da40ea3d03667b22cac120b2a9
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2020-12-02 21:23:35 -0300

    Change background-color on display_errors helper

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,12 +1,11 @@
 module UserHelper
   def display_errors(user)
-    
-    if user.errors.any?
-      user.errors.full_messages.collect do |message|
-        content_tag(:div, class: 'bg-white text-info d-flex justify-content-center font-weight-bold') do 
-          message
-        end
-      end.join.html_safe
-    end    
+    return unless user.errors.any?
+
+    user.errors.full_messages.collect do |message|
+      content_tag(:div, class: 'bg-dark text-white d-flex justify-content-center font-weight-bold') do
+        message
+      end
+    end.join.html_safe
   end
 end
