458e28a5e57d5471923e3c2894688fd662a7df3a
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2020-12-03 19:43:21 -0300

    Fix grid problem with row size

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,6 @@ require("channels")
 //= require jquery3
 //= require popper
 //= require bootstrap
-//= require bootstrap-select
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
