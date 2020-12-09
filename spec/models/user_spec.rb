14b29fad161596e47eef391e237d9d174ca692ec
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2020-12-09 15:49:06 -0300

    Remove instance variables from tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,21 +3,21 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   context 'Create an user' do
     it 'Create a valid user' do
-      @user = User.new(name: 'Marcelo', username: 'marcelomaidden')
+      user = User.new(name: 'Marcelo', username: 'marcelomaidden')
 
-      expect(@user).to be_valid
+      expect(user).to be_valid
     end
 
     it 'Validates user without a name' do
-      @user = User.new(username: 'marcelomaidden')
+      user = User.new(username: 'marcelomaidden')
 
-      expect(@user).to_not be_valid
+      expect(user).to_not be_valid
     end
 
     it 'Validates user without username' do
-      @user = User.new(name: 'Marcelo')
+      user = User.new(name: 'Marcelo')
 
-      expect(@user).to_not be_valid
+      expect(user).to_not be_valid
     end
   end
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -10,21 +10,21 @@ RSpec.describe Vote, type: :model do
 
   context 'Register a vote' do
     it 'Creates a valid vote' do
-      @vote = Vote.new(user_id: 1, article_id: 1)
+      vote = Vote.new(user_id: 1, article_id: 1)
 
-      expect(@vote).to be_valid
+      expect(vote).to be_valid
     end
 
     it 'Validates if user is present' do
-      @vote = Vote.new(article_id: 1)
+      vote = Vote.new(article_id: 1)
 
-      expect(@vote).to_not be_valid
+      expect(vote).to_not be_valid
     end
 
     it 'Validates if article is present' do
-      @vote = Vote.new(article_id: 1)
+      vote = Vote.new(article_id: 1)
 
-      expect(@vote).to_not be_valid
+      expect(vote).to_not be_valid
     end
 
     it 'Throws error if user has already voted on the same article' do
